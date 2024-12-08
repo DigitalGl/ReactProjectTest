92e4eb0771adb13ed5df0fb3ff47aa51733c7bc7
Daniil <daniil424342@gmail.com>
2024-12-08 14:09:39 +0500

    ff

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 
-let buttonText = 'Click me please'
-const onButtonClick = () => {
-    buttonText = 'Hello from React'
-    console.log(buttonText)
-}
 
-const App = (
-    <div className="app">
-        <button onClick={onButtonClick}>
-            {buttonText}
-        </button>
-    </div>
-)
+
+const App = () => {
+    console.log('Called')
+    let buttonText = 'Click me please'
+    const onButtonClick = () => {
+        buttonText = 'Hello from React'
+        console.log(buttonText)
+    }
+    return (
+        <div className="app">
+            <button onClick={onButtonClick}>{buttonText}</button>
+        </div>
+    )
+}
 
 const container = document.getElementById('app')
 const root = ReactDOM.createRoot(container)
-root.render(App)
+root.render(<App />)
